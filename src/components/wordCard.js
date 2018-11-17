cd1489eaf5b048120f4ca4b7f7996c5603b3ed0e
paulcysu <su.paul.cy@gmail.com>
2018-11-16 20:36:39 -0800

    saving before carousel add in

--- src/components/wordCard.js
+++ src/components/wordCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, Image, ScrollView, StyleSheet } from 'react-native'
+import { Text, View, Image, ScrollView, StyleSheet, TouchableOpacity } from 'react-native'
 
 class WordCard extends Component {
 
@@ -8,18 +8,35 @@ class WordCard extends Component {
             const card = this.props.book.word_list.map((word) => {
                 return (
                     <View style={styles.cardContainer}>
-                        <View>
-                            <Text>Sound Button</Text>
+                        <View style={styles.cardHeader}>
+                            <View style={styles.arrowContainer}>
+                                <Text style={styles.arrow}>{"\<"}</Text>
+                            </View>
+                            <Image 
+                                style={{width: 75, height: 100, margin: 20}}
+                                source={{uri: "https://i.harperapps.com/covers/9780061244186/y648.jpg"}}/>
+                            <View style={styles.bookInfo}>
+                                <Text>{this.props.title}</Text>
+                                <Text>{this.props.author}</Text>
+                            </View>
+                            <View style={styles.arrowContainer}>
+                                <Text style={styles.arrow}>{"\>"}</Text>
+                            </View>
                         </View>
-                        <View>
-                            <Text>{word.word}</Text>
+                        <View style={styles.wordContainer}>
+                            <TouchableOpacity>
+                                <Text style={styles.soundButton}>Sound Button</Text>
+                            </TouchableOpacity>
+                            <View>
+                                <Text style={styles.word}>{word.word}</Text>
+                            </View>
+                            <View>
+                                <Text>{word.type} | {word.punctuation} </Text>
+                            </View>
                         </View>
-                        <View>
-                            <Text>{word.part_of_speech} | {word.punctuation} </Text>
-                        </View>
-                        <View>
-                            <Text>{word.definition}</Text>
-                            <Text>{word.example}</Text>
+                        <View style={styles.definition}>
+                            <Text>Definition: {word.definition}</Text>
+                            <Text>Example: {word.example}</Text>
                         </View>
                     </View>
                 )
@@ -35,21 +52,6 @@ class WordCard extends Component {
   render() {
     return (
         <View style={styles.container}>
-            <View style={styles.cardHeader}>
-                <View style={styles.arrowContainer}>
-                    <Text style={styles.arrow}>{"\<"}</Text>
-                </View>
-                <Image 
-                    style={{width: 75, height: 100, margin: 20}}
-                    source={{uri: "https://i.harperapps.com/covers/9780061244186/y648.jpg"}}/>
-                <View style={styles.bookInfo}>
-                    <Text>{this.props.title}</Text>
-                    <Text>{this.props.author}</Text>
-                </View>
-                <View style={styles.arrowContainer}>
-                    <Text style={styles.arrow}>{"\>"}</Text>
-                </View>
-            </View>
             <ScrollView>
                 {this.renderCards()}
             </ScrollView>
@@ -66,7 +68,8 @@ const styles = StyleSheet.create({
         borderRadius: 5
     },
     cardContainer: {
-        padding: 20,
+        borderWidth: 1,
+        borderColor: "black"
     },
     cardHeader: {
         flexDirection: "row",
@@ -82,6 +85,22 @@ const styles = StyleSheet.create({
     },
     arrow: {
         fontSize: 25
+    },
+    word: {
+        padding: 10,
+        fontSize: 30,
+        fontWeight: "bold"
+    },
+    wordContainer: {
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    soundButton: {
+        fontWeight: "bold"
+    },
+    definition: {
+        paddingTop: 30,
+        padding: 10
     }
 })
 
