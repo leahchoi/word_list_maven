82ad1bde30f1068dd27a2cb6db001699db03bfd4
leahchoi <soora913@gmail.com>
2018-11-17 14:26:18 -0800

    rendered one book list title

--- App.js
+++ App.js
@@ -37,7 +37,7 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    paddingTop: 80,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
