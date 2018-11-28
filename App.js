1ff1e76f1e36822932a201c66537706ae612dce8
paulcysu <su.paul.cy@gmail.com>
2018-11-28 14:54:17 -0800

    merge completd

--- App.js
+++ App.js
@@ -5,10 +5,6 @@ import { Provider , connect } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
 import reducers from './src/reducers'
 
-import reducers from './src/reducers/index';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux'
-
 type Props = {};
 
 class App extends Component<Props> {
@@ -19,37 +15,6 @@ class App extends Component<Props> {
       selectedWordComplete: false,
       carouselModalVisible: false
   };
-
-// const store= createStore(reducers, state, enhancer)
-
-
-  // componentDidMount = () => {
-  //   this.getBookList();
-  // }
-
-  // getBookList = () => {
-  //   axios.get("http://localhost:3000/books")
-  //     .then(resp => {
-  //       this.setState({
-  //         book: resp.data[0]
-  //       })
-  //     })
-  // }
-
-  // onSelectHandler = (state) => {
-  //   if (!this.state.modalVisible) {
-  //     this.setState({
-  //       selectedWord: state,
-  //       modalVisible: true
-  //     })
-  //   } else {
-  //     this.setState({
-  //       selectedWord: null,
-  //       modalVisible: null
-  //     })
-  //   }
-  // }
-
 
   toggleComplete = () => {
     this.setState({
